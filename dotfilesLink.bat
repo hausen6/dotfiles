69995d196f01c51acd3f6172e793dea0c16d11d7
hausen6 <hausen62480@gmail.com>
2014-05-16 12:15:57 +0900

    無視リストの追加とwin用バッチのをスニペットフォルダにも対応

--- dotfilesLink.bat
+++ dotfilesLink.bat
@@ -3,29 +3,36 @@
 
 rem *********** pathのセット **************
 rem vimrcのパスをセット
-set VIMRC=%~p0_vimrc
+set VIMRC=%~dp0_vimrc
 echo %VIMRC%
 rem gvimrcのパスをセット
-set GVIMRC=%~p0_gvimrc
+set GVIMRC=%~dp0_gvimrc
 echo %GVIMRC%
 rem .latexmkrcのパスをセット
-set latexmkrc=%~p0.latexmkrc
+set latexmkrc=%~dp0.latexmkrc
 echo %latexmkrc%
+rem スニペット用フォルダ mysnip のパスをセット
+set MYSNIP=%~dp0mysnip
+echo %MYSNIP%
+echo %~dp0
 rem *********** pathのセット **************
 
 rem ディレクトリ移動
+echo %USERPROFILE%
 cd %USERPROFILE%
 
 rem 既にリンクが作成されていた場合はそのリンクを削除
 if exist _vimrc del _vimrc
 if exist _gvimrc del _gvimrc
 if exist .latexmkrc del .latexmkrc
+if exist .vim\mysnip rmdir .vim\mysnip
 
 :mklink
 rem シンボリックリンクを作成
 mklink _vimrc %VIMRC%
 mklink _gvimrc %GVIMRC%
 mklink .latexmkrc %latexmkrc%
+mklink /d .vim\mysnip %MYSNIP%
 
 PAUSE
 exit
