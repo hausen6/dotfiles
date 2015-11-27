3dfe51528d4b0081e74f3f42f0dd90c4bdd7dbaa
hausen6 <hausen62480@gmail.com>
2015-11-28 02:07:16 +0900

    brew_fileの設定を追加

--- etc/init/osx/01_brew_file.sh
+++ etc/init/osx/01_brew_file.sh
@@ -7,7 +7,13 @@ source "$DOTPATH"/etc/lib/utils.sh
 
 if ! has "brew"; then
 	log_fail "brew-file must be required to brew"
-	exit
+	exit 1
 fi
 
 brew install rcmdnk/file/brew-file
+if [ $? -eq 0 ]; then
+	export HOMEBREW_BREWFILE=$(cd $(dirname $0) && pwd)/brew_file.txt
+else
+	log_fail "brew-file cloud not install"
+	exit 1
+fi
