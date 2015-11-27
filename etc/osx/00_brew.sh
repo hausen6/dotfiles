6483c69e44f69f309fcdd0fe557f724966dc26d7
hausen6 <hausen62480@gmail.com>
2015-11-27 19:59:43 +0900

    より便利にdotfileを管理するための第一歩を踏み出した
    便利関数を記述している dotfiles/etc/lib/utils.shの追加
    mac用のbrewインストールスクリプトの追加

---
+++ etc/osx/00_brew.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# homebrew をインストールするためのスクリプト
+trap 'echo ERROR: $0:$LINENO' SIGINT SIGTERM
+set -eu
+
+source "$DOTPATH"/etc/lib/utils.sh
+
+if has "brew"; then
+	log_pass "already installed brew"
+	exit
+fi
+
+# install brew
+log_echo "brew install with ruby"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+if has "brew"; then
+	brew doctor
+else
+	log_fail "ERROR: brew failed to install"
+	exit 1
+fi
+
+log_pass "brew successfully installed"
